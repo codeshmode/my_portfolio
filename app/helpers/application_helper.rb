92f1771638f4bfebcf1d4ff455e454d87fcd3466
Eddie Mares <eddiem.@eddies-mbp.attlocal.net>
2018-05-01 21:14:54 -0700

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting"  )
     end
   end
+
+  def copyright_generator
+    @copyright = SeasViewTool::Renderer.copyright 'Eddie Mares', 'All rights reserved'
+  end
 end
